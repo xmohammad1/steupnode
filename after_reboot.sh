7c722e48a1f382aa2be3718e84c84b0afdc89750
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 04:07:11 +0200

    Update after_reboot.sh

--- after_reboot.sh
+++ after_reboot.sh
@@ -8,8 +8,5 @@ nameserver 9.9.9.9
 nameserver 127.0.0.53
 EOL
 tmux new-session -d -s update-session 'bash <(curl -u xmohammad1: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 -LS https://raw.githubusercontent.com/xmohammad1/steupnode/main/node.sh) install'
-tmux attach -t update-session
-# Wait until the tmux session ends (node setup ends)
-tmux wait-for -S update-session
 # Remove the cron job to ensure this script only runs once
 crontab -l | grep -v "@reboot /root/after_reboot.sh" | crontab -
