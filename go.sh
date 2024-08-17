fb6aecef013f201bee216706fbf222fec8d5d1b7
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 05:07:57 +0200

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,8 @@
+if [ -f /root/after_reboot.sh ]; then
+    echo "Script runned once already"
+    exit 1
+fi
+SCRIPT_PATH="/root/main.sh"
+sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/main.sh -O /root/main.sh
+chmod +x "$SCRIPT_PATH"
+bash $SCRIPT_PATH
