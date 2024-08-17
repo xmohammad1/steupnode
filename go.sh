ae8d2de850dee315f5453703a2ba98605e7cc926
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 05:16:47 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,5 +5,5 @@ fi
 SCRIPT_PATH="/root/main.sh"
 sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/main.sh -O $SCRIPT_PATH
 chmod +x "$SCRIPT_PATH"
-tmux new-session -d -s update-session 'bash $SCRIPT_PATH'
+tmux new-session -d -s update-session 'bash /root/main.sh'
 tmux attach -t update-session
