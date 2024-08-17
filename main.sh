77ce36948e00cb1e26afc1be18d7a03c4a4134ec
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 03:37:32 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,19 @@
 tmux new-session -d -s update-session 'sudo apt update -y && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && bash <(curl -LS https://raw.githubusercontent.com/xmohammad1/bbr/main/bbr.sh)'
 tmux attach -t update-session
-echo "Wait 20Sec to make sure all updates done"
-sleep 20
+
+# Wait until the tmux session ends (updates completed)
+tmux wait-for -S update-session
+
 # Path to the script you want to run after reboot
 SCRIPT_PATH="/root/after_reboot.sh"
-sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/after_reboot.sh -O $SCRIPT_PATH
+sudo wget --header="Authorization: token $GITHUB_TOKEN" https://raw.githubusercontent.com/xmohammad1/steupnode/main/after_reboot.sh -O "$SCRIPT_PATH"
 # Ensure the script is executable
 chmod +x "$SCRIPT_PATH"
+
 # Define the cron job
-CRON_JOB="@reboot $SCRIPT_PATH"
+CRON_JOB="@reboot sudo $SCRIPT_PATH"
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 echo "Cron job added: $CRON_JOB"
-reboot
+sleep 15
+# Reboot the system
+sudo reboot
