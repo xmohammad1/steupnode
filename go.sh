1905ce76602a21400ace0a7eca86f1a5769286a0
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 05:14:58 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -3,7 +3,7 @@ if [ -f /root/after_reboot.sh ]; then
     exit 1
 fi
 SCRIPT_PATH="/root/main.sh"
-sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/main.sh -O /root/main.sh
+sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/main.sh -O $SCRIPT_PATH
 chmod +x "$SCRIPT_PATH"
 tmux new-session -d -s update-session 'bash $SCRIPT_PATH'
 tmux attach -t update-session
