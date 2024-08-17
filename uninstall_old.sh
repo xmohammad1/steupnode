f246606b46bfd52483baeba20ee01fa1b385ea90
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-17 07:48:56 +0200

    Update uninstall_old.sh

--- uninstall_old.sh
+++ uninstall_old.sh
@@ -1,5 +1,4 @@
-docker compose down --remove-orphans
-
+docker compose -f /root/Marzban-node/docker-compose.yml down --remove-orphans
 rm -rf /var/lib/marzban-node
 rm -rf /root/Marzban-node
 rm -rf /var/lib/marzban
