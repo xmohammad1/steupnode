18a6046a74ff26e082f7301165f4dcf863d7466f
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-23 04:39:55 +0330

    Update after_reboot.sh

--- after_reboot.sh
+++ after_reboot.sh
@@ -8,16 +8,15 @@ nameserver 78.157.42.100
 nameserver 78.157.42.101
 EOL
 fi
-tmux new-session -d -s update-session 'bash <(curl -u xmohammad1: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 -LS https://raw.githubusercontent.com/xmohammad1/steupnode/main/node.sh) install'
+bash <(curl -u xmohammad1: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 -LS https://raw.githubusercontent.com/xmohammad1/steupnode/main/node.sh) install
 # Remove the cron job to ensure this script only runs once
 sudo crontab -l | grep -v '@reboot sudo /root/after_reboot.sh' | sudo crontab -
-
+bash <(curl -LS https://raw.githubusercontent.com/xmohammad1/linux_security/main/main.sh)
 # Remove the existing /etc/resolv.conf file
 rm /etc/resolv.conf
 # Create a new /etc/resolv.conf file
 cat <<EOL > /etc/resolv.conf
 nameserver 1.1.1.1
 nameserver 8.8.8.8
-nameserver 9.9.9.9
 nameserver 127.0.0.53
 EOL
