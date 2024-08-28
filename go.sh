5827c947e2bd1f43b854fab6aa426630f43a83d3
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-28 10:37:32 +0330

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,6 +5,7 @@ FILE="/root/config.cfg"
 if [ -f "$FILE" ]; then
     echo "The file $FILE exists."
 elif [ -z "${Link}" ]; then
+    wget https://raw.githubusercontent.com/xmohammad1/steupnode/main/config.cfg -O $FILE
     # Prompt the user to input the certificate
     echo -e "Please paste the content of the Client Certificate, and press ENTER on a new line when finished:"
     
