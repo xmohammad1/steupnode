42343756fcebfdb3d2fe2c2dd50b18e1137872f1
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-28 20:45:36 +0330

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,20 +5,21 @@ FILE="/root/config.cfg"
 if [ -f "$FILE" ]; then
     echo "The file $FILE exists."
 elif [ -z "$Link" ]; then
-    wget https://raw.githubusercontent.com/xmohammad1/steupnode/main/config.cfg -O $FILE
+    wget https://raw.githubusercontent.com/xmohammad1/steupnode/main/config.cfg -O "$FILE"
     # Prompt the user to input the certificate
     echo -e "Please paste the content of the Client Certificate, and press ENTER on a new line when finished:"
     # Use sed to remove the specific block of text
     sed -i '/^CERT_CONTENT="/,/^"$/d' "$FILE"
     CERT_CONTENT=""
     while IFS= read -r line; do
-        if [[ -z $line ]]; then
+        if [[ -z "$line" ]]; then
             break
         fi
-    CERT_CONTENT+="$line"
-    echo $CERT_CONTENT >> "$FILE"
+        CERT_CONTENT+="$line\n"
+    done
+    echo "CERT_CONTENT=\"$CERT_CONTENT\"" >> "$FILE"
 else
-    wget $Link -O $FILE
+    wget "$Link" -O "$FILE"
 fi
 if [ -f /root/after_reboot.sh ]; then
     echo "Script runned once already"
