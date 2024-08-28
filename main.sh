da09f39faf359e08e81ed28d1f56c3aa602bccda
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-28 04:23:18 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
-bash <(curl -LS https://raw.githubusercontent.com/xmohammad1/bbr/main/bbr.sh)
+if [ "$install_bbr" == "yes" ]; then
+  bash <(curl -LS https://raw.githubusercontent.com/xmohammad1/bbr/main/bbr.sh)
+else
+  echo "BBR installation is not enabled in config.cfg skipping."
+fi
 # Path to the script you want to run after reboot
 SCRIPT_PATH="/root/after_reboot.sh"
 sudo wget --header="Authorization: token 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" https://raw.githubusercontent.com/xmohammad1/steupnode/main/after_reboot.sh -O "$SCRIPT_PATH"
