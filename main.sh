d3a21d30fcf1ef9363ec5bfaa285dde404fdd598
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-28 06:01:10 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,7 +13,7 @@ sudo wget --header="Authorization: token ghp_Asyp8iuKqZxqfrIvTFi1aD2Po0N6qT4Jmb5
 sudo chmod +x "$SCRIPT_PATH"
 
 # Define the cron job
-CRON_JOB="@reboot tmux new-session -d -s update-session 'bash /root/after_reboot.sh' | bash"
+CRON_JOB="@reboot tmux new-session -d -s update-session 'bash /root/after_reboot.sh; exec bash'"
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 echo "Cron job added: $CRON_JOB"
 echo "Waiting 15 Sec before reboot"
